660ded998f0697dfeb1397865805c97570952bb2
PewDizinho <konopkspaulo046@gmail.com>
2024-04-06 02:46:20 -0300

    some desktop stuff

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let shouldKeepGoing = false;
+var typing = new Audio('./assets/typing.mp3');
 let init = async () => {
     let details = {
         email: "pewdizinho@hotmail.com",
@@ -6,24 +8,25 @@ let init = async () => {
     let emailInput = document.getElementsByTagName("input")[0];
     let passwordInput = document.getElementsByTagName("input")[1];
     emailInput.value = "";
-    var audio = new Audio('./assets/typing.mp3');
+    passwordInput.value = "";
+    if (!shouldKeepGoing) return;
     await delay(5500);
-    audio.play();
+    if (!shouldKeepGoing) return;
+    typing.play();
     for (var letter of details.email.split("")) {
         await delay(100);
+        if (!shouldKeepGoing) return;
         emailInput.value += letter;
     }
     for (var letter of details.password.split("")) {
         await delay(100);
+        if (!shouldKeepGoing) return;
         passwordInput.value += letter;
     }
-    audio.pause();
-    audio.currentTime = 0;
+    typing.pause();
+    typing.currentTime = 0;
     await delay(5000);
-    // var newAudio = (new Audio('./assets/mouseclick.mp3'));
-    // newAudio.play();
-    console.log("platyed")
-    // await delay(1000)
+    if (!shouldKeepGoing) return;
     sendEmail();
 };
 const delay = ms => new Promise(res => setTimeout(res, ms));
@@ -32,7 +35,7 @@ const changeIt = () => {
     document.getElementById("mainPage").style.display = "";
     var audio = new Audio('./assets/startup.mp3');
     audio.play();
-    init();
+
 }
 const sendEmail = () => {
     var newAudio = (new Audio('./assets/mouseclick.mp3'));
@@ -41,5 +44,31 @@ const sendEmail = () => {
         location.href = "./homeScreen/index.html";
 
     }, 1000)
+
+}
+
+const clickMsn = () => {
+    const msn = document.getElementById("msn");
+    const msnTaskBar = document.getElementById("msnTaskBar")
+    var newAudio = (new Audio('./assets/mouseclick.mp3'));
+    newAudio.play();
+    setTimeout(() => {
+        if (msn.style.display == "none") {
+            msn.style.display = "";
+            msnTaskBar.style.backgroundColor = "rgba(255, 255, 255, .3)";
+            msnTaskBar.style.borderBottom = "1px solid rgba(255, 255, 255, .3)";
+            shouldKeepGoing = true;
+            init();
+
+        } else {
+            msnTaskBar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+            msnTaskBar.style.borderBottom = "1px solid rgba(0, 0, 0, 0)";
+            msn.style.display = "none";
+            typing.pause();
+            typing.currentTime = 0;
+            shouldKeepGoing = false;
+
+        }
+    }, 200); //TODO: Diminuir o tamanho do audio de click
 
 }
